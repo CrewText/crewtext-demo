c79534328ee73a1802fd79787db8f7af22fc1ec4
Cal McLean <calmcl1@aol.com>
2020-04-05 18:19:46 +0100

    update env vars in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   auth_callback_url: "https://voluble-poc-site.herokuapp.com/auth/callback",
   auth0_client_id: "COMAy4nBatqEuGzdTHEzOOc2ucpRywcs",
-  auth0_proxy_base: "http://localhost:5000",
+  auth0_proxy_base: "https://voluble-poc.herokuapp.com",
   voluble: {
-    api_base: "https://voluble-poc.herokuapp.com",
+    api_base: "https://voluble-poc.herokuapp.com/v1",
   }
 };
